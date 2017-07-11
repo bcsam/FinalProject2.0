95a6d53e0536422ebb30ac88f05c741597956bd4
Vivian Fang <vf608@fb.com>
2017-07-11 16:01:20 -0700

    get tone scores

--- app/src/main/java/com/codepath/finalproject/MainActivity.java
+++ app/src/main/java/com/codepath/finalproject/MainActivity.java
@@ -2,18 +2,25 @@ package com.codepath.finalproject;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
     Context context;
+    AnalyzerClient client;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
         //Button btCheck = (Button) findViewById(btCheck);
-
+        client = new AnalyzerClient();
+        client.getToneScores("I am so angry!");
     }
 
    /*
