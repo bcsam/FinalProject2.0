8dd3e36395be98e0c0b3a601102ee1d26f6bbc85
Brent Samuels <bcsam@fb.com>
2017-07-12 10:14:06 -0700

    Template added

--- app/src/main/java/com/codepath/finalproject/PostCheckActivity.java
+++ app/src/main/java/com/codepath/finalproject/PostCheckActivity.java
@@ -2,8 +2,15 @@ package com.codepath.finalproject;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by bcsam on 7/11/17.
@@ -14,7 +21,7 @@ public class PostCheckActivity extends AppCompatActivity {
     TextView tvBody;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_post_check);
         String text = getIntent().getStringExtra("text");
@@ -25,6 +32,50 @@ public class PostCheckActivity extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.tvTextBody);
         textView.setText(text);
         textView.setTextColor(Color.parseColor(textBody.getColor()));
+
+        //new code for tabs
+
+        // Locate the viewpager in activity_main.xml
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+
+        // Set the ViewPagerAdapter into ViewPager
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new LeftFragment(), "Players");//need to replace with fragments
+        adapter.addFrag(new RightFragment(), "Prizes");
+
+        viewPager.setAdapter(adapter);
+
+        TabLayout mTabLayout = (TabLayout) findViewById(R.id.pager_header);
+        mTabLayout.setupWithViewPager(viewPager);
+    }
+        class ViewPagerAdapter extends FragmentStatePagerAdapter {
+            private final List<Fragment> mFragmentList = new ArrayList<>();
+            private final List<String> mFragmentTitleList = new ArrayList<>();
+
+            public ViewPagerAdapter(FragmentManager manager) {
+                super(manager);
+            }
+
+            @Override
+            public Fragment getItem(int position) {
+                return mFragmentList.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mFragmentList.size();
+            }
+
+            public void addFrag(Fragment fragment, String title) {
+                mFragmentList.add(fragment);
+                mFragmentTitleList.add(title);
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return mFragmentTitleList.get(position);
+            }
+        }
     }
 /*<<<<<<< HEAD
 =======
