17c0ec0524a7b4dadfb52800a354558b6750a641
Brent Samuels <bcsam@fb.com>
2017-07-14 13:09:29 -0700

    error gone in main activity

--- app/src/main/java/com/codepath/finalproject/MainActivity.java
+++ app/src/main/java/com/codepath/finalproject/MainActivity.java
@@ -140,27 +140,5 @@ public class MainActivity extends ListActivity { // TODO: 7/12/17 make the app w
         return contactName;
     }
 
-    public String getContactName(final String phoneNumber,Context context)
-
-    {
-
-        Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI,Uri.encode(phoneNumber));
-
-        String[] projection = new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME};
-
-        String contactName = "";
-        Cursor cursor = context.getContentResolver().query(uri,projection,null,null,null);
-
-        if(cursor.moveToFirst())
-        {
-
-            contactName=cursor.getString(0);
-
-        }
-        cursor.close();
-        cursor = null;
-
-        return contactName;
-    }
 }
 
