2df36c4b87126abf23a0551348db1d6c727aa5ad
Vivian Fang <vf608@fb.com>
2017-07-18 14:55:25 -0700

    colored texts

--- app/src/main/java/com/codepath/finalproject/ListAdapter.java
+++ app/src/main/java/com/codepath/finalproject/ListAdapter.java
@@ -2,6 +2,7 @@ package com.codepath.finalproject;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.StrictMode;
 import android.support.v7.widget.RecyclerView;
 import android.telephony.TelephonyManager;
@@ -65,9 +66,9 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder>{
         }
         TextBody textBody = new TextBody();
         textBody.setMessage(body);
-        //client.getToneScores(textBody);
+        client.getToneScores(textBody);
         holder.tvBody.setText(body);
-        //holder.tvBody.setTextColor(Color.parseColor(textBody.getTextColor()));
+        holder.tvBody.setTextColor(Color.parseColor(textBody.getTextColor()));
         holder.date.setText(date);
         holder.ivProfileImage.setOnClickListener(new View.OnClickListener() {
             @Override
